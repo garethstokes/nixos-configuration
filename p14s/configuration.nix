e512c971e7b7588fbf1feba57870645b73badfed
gareth <gareth.stokes@paidright.io>
2022-09-11 10:08:10 +1000

    latest changes to configuration.nix

--- p14s/configuration.nix
+++ p14s/configuration.nix
@@ -3,6 +3,15 @@
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 { config, pkgs, ... }:
+let
+  nvidia-offload = pkgs.writeShellScriptBin "nvidia-offload" ''
+    export __NV_PRIME_RENDER_OFFLOAD=1
+    export __NV_PRIME_RENDER_OFFLOAD_PROVIDER=NVIDIA-G0
+    export __GLX_VENDOR_LIBRARY_NAME=nvidia
+    export __VK_LAYER_NV_optimus=NVIDIA_only
+    exec "$@"
+  '';
+in
 {
   imports =
     [ # Include the results of the hardware scan.
@@ -34,10 +43,17 @@
 
   hardware.nvidia = {
     package = config.boot.kernelPackages.nvidiaPackages.legacy_470;
+    # package = config.boot.kernelPackages.nvidiaPackages.stable;
+    # open = true;
+
     modesetting.enable = true;
+
     powerManagement.enable = true;
+    powerManagement.finegrained = true;
+
     prime = {
       offload.enable = true;
+      # sync.enable = true;
       intelBusId = "PCI:0:2:0";
       nvidiaBusId = "PCI:3:0:0";
     };
@@ -71,6 +87,7 @@
 
     systemPackages = with pkgs; [
       wget
+      nvidia-offload
 
       # basic vim base install
       (neovim.override {
@@ -200,6 +217,9 @@
   services.logind = {
     lidSwitch = "ignore";
   };
+
+  # firmware updater
+  services.fwupd.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
