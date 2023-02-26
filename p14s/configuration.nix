7c41e67a945f1481f05e36120746f9244ecbaafa
gareth <gareth.stokes@paidright.io>
2023-02-26 20:17:05 +1100

    use 6_1_hardened'

--- p14s/configuration.nix
+++ p14s/configuration.nix
@@ -31,7 +31,7 @@ in
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_6_1_hardened;
   # boot.kernelParams = [ "module_blacklist=i915" ];
 
   networking.hostName = "bandit"; # Define your hostname.
