543b114941cbb59b5dcd577ea0f7eed7642cae58
gareth <gareth.stokes@paidright.io>
2023-02-26 20:12:19 +1100

    Revert "Revert "patch evdi to work with linux kernel 6.0""
    This reverts commit e1a71f29c6128ac7719ee47e9da0794aec9aa776.

--- p14s/configuration.nix
+++ p14s/configuration.nix
@@ -31,7 +31,16 @@ in
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelPackages = pkgs.linuxPackages_latest.extend (self: super: {
+    evdi = super.evdi.overrideAttrs (o: rec {
+      src = pkgs.fetchFromGitHub {
+        owner = "DisplayLink";
+        repo = "evdi";
+        rev = "bdc258b25df4d00f222fde0e3c5003bf88ef17b5";
+        sha256 = "mt+vEp9FFf7smmE2PzuH/3EYl7h89RBN1zTVvv2qJ/o=";
+      };
+    });
+  });
   # boot.kernelParams = [ "module_blacklist=i915" ];
 
   networking.hostName = "bandit"; # Define your hostname.
