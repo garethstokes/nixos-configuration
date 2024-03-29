65029925b009a151d29a6f43e7ee533db49c6bd0
gareth <gareth.stokes@paidright.io>
2024-03-29 15:38:23 +1100

    add trouble to nvim

--- home-manager/nvim/lua/plugins/lualine.lua
+++ home-manager/nvim/lua/plugins/lualine.lua
@@ -1,84 +1,87 @@
 local icons = require("lazyvim.config").icons
 
 local diff = {
-    "diff",
-    symbols = {
-        added = icons.git.added,
-        modified = icons.git.modified,
-        removed = icons.git.removed,
-    },
+  "diff",
+  symbols = {
+    added = icons.git.added,
+    modified = icons.git.modified,
+    removed = icons.git.removed,
+  },
 }
 
 local diagnostic = {
-    "diagnostics",
-    symbols = {
-        error = icons.diagnostics.Error,
-        warn = icons.diagnostics.Warn,
-        info = icons.diagnostics.Info,
-        hint = icons.diagnostics.Hint,
-    },
+  "diagnostics",
+  symbols = {
+    error = icons.diagnostics.Error,
+    warn = icons.diagnostics.Warn,
+    info = icons.diagnostics.Info,
+    hint = icons.diagnostics.Hint,
+  },
 }
 
 local filename = {
-    "filename",
-    cond = function()
-        local bufs_loaded = {}
-
-        for i, buf_hndl in ipairs(vim.api.nvim_list_bufs()) do
-            if vim.api.nvim_buf_is_loaded(buf_hndl) then
-                bufs_loaded[i] = buf_hndl
-            end
-        end
-
-        return #bufs_loaded == 2
-    end,
-    symbols = {
-        modified = " ",
-        readonly = "[ro]",
-        unnamed = "[unnamed]",
-        newfile = "[new]",
-    },
-}
-
-local navic = {
-    function()
-        return require("nvim-navic").get_location()
-    end,
-    cond = function()
-        return package.loaded["nvim-navic"] and require("nvim-navic").is_available()
-    end,
+  "filename",
+  path = 0, -- set to 0 for just filename
+  symbols = {
+      modified = "",
+      readonly = "[ro]",
+      unnamed = "[No Name]",
+      newfile = "[New]",
+  },
 }
 
 local position = {
-    "location",
-    padding = { left = 1, right = 1 },
+  "location",
+  padding = { left = 0, right = 1 },
+}
+
+local trouble = {
+  function() 
+    return require("trouble").statusLine({
+      mode = "lsp_document_symbols",
+      groups = {},
+      title = false,
+      filter = { range = true },
+      format = "{kind_icon}{symbol.name:Normal}",
+    })
+  end
+}
+
+local filetype = {
+  "fileformat",
+  padding = { left = 1, right = 0 }
+}
+
+local encoding = {
+  "encoding",
+  padding = { left = 1, right = 0 }
 }
 
 return {
-    {
-        "nvim-lualine/lualine.nvim",
-        event = "VeryLazy",
-        opts = function()
-            return {
-                options = {
-                    -- component_separators = { left = "╲", right = "│" },
-                    -- section_separators = { left = "", right = "" },
-                    component_separators = { left = "│", right = "│" },
-                    section_separators = { left = "", right = "" },
-                    theme = "auto",
-                    globalstatus = true,
-                    disabled_filetypes = { statusline = { "dashboard", "alpha" } },
-                },
-                sections = {
-                    lualine_a = { "mode" },
-                    lualine_b = { "branch", diff },
-                    lualine_c = { diagnostic, navic },
-                    lualine_x = { "searchcount", "selectioncount", "encoding", "filetype" },
-                    lualine_y = { filename },
-                    lualine_z = { position },
-                },
-                extensions = { "neo-tree", "lazy" },
-            }
-        end,
-    },
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    opts = function()
+      return {
+        options = {
+          -- component_separators = { left = "╲", right = "│" },
+          -- section_separators = { left = "", right = "" },
+          component_separators = { left = "", right = "" },
+          -- section_separators = { left = "|", right = "|" },
+          theme = "auto",
+          globalstatus = true,
+          disabled_filetypes = { statusline = { "dashboard", "alpha" } },
+        },
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = { "branch", diff },
+          lualine_c = { filetype, filename },
+          lualine_x = { "searchcount", "selectioncount" },
+          lualine_y = { trouble, diagnostic, encoding, "filesize" },
+          lualine_z = { position },
+        },
+        extensions = { "neo-tree", "lazy" },
+      }
+    end,
+  },
 }
